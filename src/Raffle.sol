d5aa4aa9bb2487a207db05350772b50406d642e5
SphinxBSD <fav6519@gmail.com>
2025-07-17 14:29:58 -0400

    basic skeleton

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,17 +8,31 @@ pragma solidity ^0.8.13;
  * @dev It implments Chainlink VRF2.5 and Chainlink Automation
  */
 contract Raffle {
+    /** Errors */
+    error Raffle__NotEnoughEthForRuffle();
+    
+    /** Local Variables */
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public {
-        
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, NotEnoughEthForRuffle());
+        if (msg.value < i_entranceFee) revert Raffle__NotEnoughEthForRuffle();
+
     }
 
     function pickWinner() public {
         
+    }
+
+    /**
+     * Getter Functions
+     */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
     }
 }
